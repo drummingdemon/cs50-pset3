5cac99420d3f2c197067cc0fcca7b7343edd4436
Gábor Hargitai <drummingdemon@users.noreply.github.com>
2015-11-15 18:08:30 +0100

    Binary Search was totally off
    Note to self: never try to implement something while being half asleep

--- helpers.c
+++ helpers.c
@@ -14,7 +14,46 @@
 
 /**
  * Returns true if value is in array of n values, else false.
+ * Does Linear Search
+ *
+ * Checked all green with check50: 
+ * https://sandbox.cs50.net/checks/a814b06b7c644323b4540a91cc2cde4d
+ */
+bool searchLinear(int value, int values[], int n)
+{
+    while (n > 0) 
+    {
+        int mid = round(n/2);
+        
+        if (values[mid] == value) return true;
+        
+        else if (values[mid] > value)
+        {
+            for (int i = mid; i > 0; i--)
+            {
+                if (values[i] == value) return true;
+            }
+        }
+            
+        else if (values[mid] < value)
+        {
+            for (int i = mid; i < n; i++)
+            {
+                if (values[i] == value) return true;
+            }
+        }
+        
+        n--;
+    }
+    return false;
+} 
+
+/*
+ * Returns true if value is in array of n values, else false.
  * Does Binary Search
+ *
+ * Checked all green with check50:
+ * https://sandbox.cs50.net/checks/362b3a508b12429ab2bada945152a231
  */
 bool search(int value, int values[], int n)
 {
@@ -23,80 +62,36 @@ bool search(int value, int values[], int n)
         return false;
     }
     
-    if (fmod(n, 2) != 0)
+    do
     {
-        do
-        {   
-            if (values[n/2] == value)
+        if (values[(n/2)] == value)
+        {
+            return true;
+        }
+        else if (values[(n/2)] > value)
+        {
+            int stack[(n/2)];
+            for (int i = 0; i < n/2; i++)
             {
-                return true;
+                stack[i] = values[i];
             }
-            
-            else if (values[n/2] < value)
-            {   
-                int stack[(n/2)-1];
-                
-                for (int i=0; i<(n/2); i++)
-                {
-                    stack[i] = values[(n/2)+i+1];
-                }
-             
-                return search(value, stack, (n/2));
-            }
-            
-            else if (values[n/2] > value)
-            {   
-                int stack[n/2];
-                
-                for (int i=0; i<(n/2); i++)
-                {
-                    stack[i] = values[i];
-                }
-               
-                return search(value, stack, (n/2));
-            }
-            
-            else return false;
+            return search(value, stack, n/2);
         }
-        while (n > 0);
-    }
-    else
-    {
-        do
-        {   
-            if (values[n/2] == value) return true;
-            
-            else if (values[n/2] < value)
-            {   
-                int stack[n/2];
-                
-                for (int i=0; i<(n/2); i++)
-                {
-                    stack[i] = values[(n/2)+i];
-                }
-             
-                return search(value, stack, (n/2));
+        else
+        {
+            int length = n - (n/2) - 1;
+            int stack[length];
+            for (int i = 0; i < length; i++)
+            {
+                stack[i] = values[(n/2)+1+i];
             }
-            
-            else if (values[n/2] > value)
-            {   
-                int stack[n/2];
-                
-                for (int i=0; i<(n/2); i++)
-                {
-                    stack[i] = values[i];
-                }
-               
-                return search(value, stack, (n/2));
-            }
-            
-            else return false;
+            return search(value, stack, length);
         }
-        while (n > 0);    
     }
-
+    while (n > 0);
+    
     return false;
-} 
+}
  
 /**
  * Sorts array of n values using Selection Sort
